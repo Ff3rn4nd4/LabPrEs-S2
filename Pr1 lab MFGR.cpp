3a7d99779ccebe4aae6b5810410ad7526ba3de0e
Fernanda Guerra <60495755+Ff3rn4nd4@users.noreply.github.com>
2020-02-06 21:15:34 -0600

    Add files via upload

---
+++ Pr1 lab MFGR.cpp
@@ -0,0 +1,14 @@
+/* Practica 1, 06-02-20 */
+
+#include <stdio.h>
+#include <conio.h>
+
+int main (){
+	
+	// imprimir mensaje simple
+	
+	printf("Hola mundo, 1927750");
+	
+	getche();
+	return 0;
+}
