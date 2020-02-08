2b72f070df3751193d717b3ec298f17fd9760ccb
Fernanda Guerra <60495755+Ff3rn4nd4@users.noreply.github.com>
2020-02-08 02:23:57 -0600

    Add files via upload
    holajjsja

---
+++ Pr2 Lab MFGR.cpp
@@ -0,0 +1,50 @@
+// Practica 2 (07-02-20)
+
+#include <stdio.h>
+#include <conio.h>
+
+int main () {
+
+		/* ENTEROS */
+		
+	//declaración de variables
+	int a =5, b =7, f;
+	
+	//operaciónes (+,-,*,/,%, ++, --)
+	
+	f= a+b;
+	printf("%d + %d = %d\n", a,b,a+b);
+	
+	// para el salto de linea se agrega despues del resultado
+	
+	f= a-b;
+	printf("%d - %d = %d\n", a,b,a-b);
+	
+	f= a*b;
+	printf("%d * %d = %d\n", a,b,a*b);
+	
+	f= a/b;
+	printf("%d / %d = %d\n", a,b,a/b);
+	
+	// reintentar
+	
+	f= a%b;
+	printf("%d % %d = %d\n", a,b,a%b);
+	
+	f= a--;
+	printf("%d - %d = %d\n", a,b,a--);
+	
+	f= b++;
+	printf("%d = %d\n",b++);
+
+		/* FLOTANTES */
+	int p= 9.2847, j: 3.5839, g;
+	
+	//operaciones (+,-,*,/,++, --, ><,==,>=, <=)
+	
+
+	getch();
+	return 0;
+
+}
+
